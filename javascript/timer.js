fbcc9c313ab0a0750359342baa4c78c9e4cb1e58
Waterfox <waterfox80@gmail.com>
2023-09-11 11:40:51 +0200

    Add the modification functionnality

--- javascript/timer.js
+++ javascript/timer.js
@@ -2,12 +2,18 @@
 let timerTime = document.getElementById('timerTime'); 
 let playButton = document.getElementById('playButton'); 
 let workingText = document.getElementById('working'); 
-let pausingText = document.getElementById('pausing')
+let pausingText = document.getElementById('pausing');
+let workInput = document.getElementById('workInput'); 
+let pauseInput = document.getElementById('pauseInput');
+let configButton = document.getElementById('configButton');
+let timerConfig = document.getElementById('timerConfig')
 
 //---Internal variable---
 let second = 0;
 let state = 'paused';
 let workState = 'working';
+let workDuration = 1500; 
+let pauseDuration = 300;
 let run;
 
 //---Function---
@@ -50,14 +56,14 @@ function addSecond(addingSecond)
             pausingText.style.textDecoration = 'underline';
             workingText.style.textDecoration = 'none';
             workState = 'pausing';
-            setSecond(300);
+            setSecond(pauseDuration);
         }
         else if(workState == 'pausing')
         {            
             pausingText.style.textDecoration = 'none';
             workingText.style.textDecoration = 'underline';
             workState = 'working';
-            setSecond(1500);
+            setSecond(workDuration);
         }
     }
     else
@@ -65,6 +71,7 @@ function addSecond(addingSecond)
         timerTime.innerHTML = secondToDuration(second);
     }
 }
+
 /**
  * Set second to the counter
  * @param {*} s second value
@@ -92,10 +99,13 @@ function play()
     }, 1000);
 }
 
+/**
+ * Reset the counter
+ */
 function reset() 
 {
     clearInterval(run)
-    setSecond(1500);
+    setSecond(workDuration);
     workingText.style.textDecoration = 'underline';
     pausingText.style.textDecoration = 'none';
     playButton.innerHTML = "<i class='fa-solid fa-play'></i>"
@@ -117,5 +127,37 @@ playButton.addEventListener('click', () =>
     }
 })
 
+workInput.addEventListener('change', (event) => {
+    workDuration = event.target.value * 60;
+    sessionStorage.setItem('pomWorkDur', event.target.value * 60);
+    reset();
+});
+
+pauseInput.addEventListener('change', (event) => {
+    pauseDuration = event.target.value * 60;
+    sessionStorage.setItem('pomPauseDur', event.target.value * 60);
+    reset();
+});
+
+configButton.addEventListener('click', () =>{
+    timerConfig.hidden = !timerConfig.hidden;
+});
+
 //---Running first automation---
+console.log(sessionStorage);
+
+if(sessionStorage.getItem('pomWorkDur'))
+{
+    workDuration = sessionStorage.getItem('pomWorkDur');
+}
+
+if(sessionStorage.getItem('pomPauseDur')) 
+{
+    pauseDuration = sessionStorage.getItem('pomPauseDur');
+}
+
+workInput.value = workDuration / 60;
+pauseInput.value = pauseDuration / 60;
+
 reset();
+
